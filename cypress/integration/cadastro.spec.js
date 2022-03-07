f2b362b6cdd465139a627874b7dcb9c586d43f27
TiDFerreira <tidferreira@rd.com.br>
2022-03-07 11:07:11 -0300

    testes com posição e tamanho dos elementos'

--- cypress/integration/cadastro.spec.js
+++ cypress/integration/cadastro.spec.js
@@ -1,6 +1,6 @@
 
 describe('Cadastro', () => {
-    it('Usuario deve se tornar um entregador', () => {
+    it('Preencher campos', () => {
         cy.viewport(1440, 900)
         cy.visit('https://buger-eats.vercel.app')
 
@@ -14,7 +14,7 @@ describe('Cadastro', () => {
             whatsapp: '11940404040',
             endereco: {
                 cep: '13471750',
-                rua: 'Rua Monaco',
+                rua: 'Rua Monaco',  
                 numero: '76',
                 complemento: '',
                 bairro: 'Vila Santa Maria',
@@ -31,5 +31,27 @@ describe('Cadastro', () => {
         cy.get('input[name="postalcode"').type(entregador.endereco.cep)
         cy.get('input[type=button][value="Buscar CEP"]').click
 
+    })
+
+    it('Tamanho Imagem - width, height', () => {
+        cy.get('img[src="/static/media/logo.e7289086.svg"]').invoke('width').should('be.eql', 200);
+        cy.get('img[src="/static/media/logo.e7289086.svg"]').invoke('height').should('be.eql', 50);
+    })
+
+    it('Tamanho Texto - font-size', () => {
+        cy.get('#page-deliver form h1').should('have.css', 'font-size', '36px');
+    })
+
+    it('Tamanho Botão - width, height', () => {
+        cy.get('button[type=submit]').invoke('width').should('be.eql', 260);
+        cy.get('button[type=submit]').invoke('height').should('be.eql', 56);
+    })
+
+    it('Posição Botão - margin-top', () => {
+        cy.get('button[type=submit]').should('have.css', 'margin-top', '40px');
+    })
+
+    it('Tamanho Input - padding', () => {
+        cy.get('input[name=name][placeholder="Nome completo"]').should('have.css', 'padding', '16px 24px');
     })
 })
